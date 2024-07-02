703b52e7b85c8f5540c61a51a05775ea5a8b3003
LucasNGRE <lucas12negre@gmail.comgit config --global user.email lucas12negre@gmail.com>
2024-07-02 11:21:44 +0200

    Add the enter possibility to add a task

--- components/AddTask.tsx
+++ components/AddTask.tsx
@@ -2,9 +2,16 @@ import { Flex, Input, Button } from "@chakra-ui/react"
 import { SmallAddIcon } from "@chakra-ui/icons"
 
 import { AddTaskProps} from '@types'
+import React from "react"
 
 
 const AddTask = ({ task, setTask, handleCreateTask}: AddTaskProps) => {
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if(e.key === 'Enter') {
+      handleCreateTask()
+    }
+  };
+
   return (
     <Flex p="2rem" pl="2rem" pr="2rem" pb="1rem">
         <Input
@@ -13,6 +20,7 @@ const AddTask = ({ task, setTask, handleCreateTask}: AddTaskProps) => {
             onChange={(e) => setTask(e.target.value)}
             value={task}
             style={{ 'background': '#fff'}}
+            onKeyDown={handleKeyPress}
         />
         <Button
             colorScheme="twitter"
